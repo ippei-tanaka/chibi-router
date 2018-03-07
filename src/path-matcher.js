decdb6f658974d16758196f531e49e99158975d8
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2018-03-07 12:54:18 -0500

    Refactor path-matcher

--- src/path-matcher.js
+++ src/path-matcher.js
@@ -2,6 +2,51 @@ import pipe from 'ramda/src/pipe';
 import defaultTo from 'ramda/src/defaultTo';
 import drop from 'ramda/src/drop';
 
+export const buildMatcher = (pattern) => {
+
+    if (typeof pattern !== 'string') {
+        throw new SyntaxError('The pattern has to be string.');
+    }
+
+    pattern = constructSafePathString(pattern);
+
+    const patternRegexp = constructPatternRegexp(pattern);
+    const paramKeys = findParameterString(pattern);
+
+    return (path) => {
+
+        if (typeof path !== 'string') {
+            throw new SyntaxError('The path has to be string.');
+        }
+
+        path = constructSafePathString(path);
+
+        const matched = path.match(patternRegexp);
+
+        if (!matched) {
+            return null;
+        }
+
+        const paramValues = drop(1, matched);
+        let params = {};
+        let wildcards = [];
+
+        if (paramValues.length > 0) {
+            paramKeys.forEach((paramKey, index) => {
+                if (paramKey === WILDCARD_SYMBOL) {
+                    wildcards.push(paramValues[index]);
+                } else {
+                    params[paramKey] = paramValues[index];
+                }
+            });
+        }
+
+        return {params, wildcards};
+    };
+};
+
+export const match = (pattern, path) => buildMatcher(pattern)(path);
+
 const WILDCARD_SYMBOL = Symbol('*');
 
 const constructSafePathString = pipe(
@@ -17,65 +62,9 @@ const findParameterString = pipe(
     a => a.map(s => s === '*' ? WILDCARD_SYMBOL : s)
 );
 
-const constructPattern = pipe(
+const constructPatternRegexp = pipe(
     s => s.replace(/:[^/\s?#&=]+/g, '([^/\\s?#&=]+)'),
     s => s.replace(/\*/g, '(.*)'),
     s => '^' + s + '\/?$',
     s => new RegExp(s)
 );
-
-const extractPattern = pattern => {
-    if (typeof pattern !== 'string')
-    {
-        throw new SyntaxError('The argument has to be string.');
-    }
-    const pathStr = constructSafePathString(pattern);
-    const regexp = constructPattern(pathStr);
-    const paramKeys = findParameterString(pathStr);
-    return {
-        regexp,
-        paramKeys
-    };
-};
-
-export const buildMatcher = (pattern) =>
-{
-    const {regexp, paramKeys} = extractPattern(pattern);
-
-    return (path) => {
-
-        if (typeof path !== 'string')
-        {
-            throw new SyntaxError('The argument should be string.')
-        }
-
-        path = constructSafePathString(path);
-
-        const matched = path.match(regexp);
-
-        if (!matched)
-        {
-            return null;
-        }
-
-        const paramValues = drop(1, matched);
-        let params = {};
-        let wildcards = [];
-
-        if (paramValues.length > 0)
-        {
-            paramKeys.forEach((key, index) => {
-                if (key === WILDCARD_SYMBOL)
-                {
-                    wildcards.push(paramValues[index]);
-                } else {
-                    params[key] = paramValues[index];
-                }
-            });
-        }
-
-        return {params, wildcards};
-    }
-};
-
-export const match = (pattern, path) => buildMatcher(pattern)(path);
