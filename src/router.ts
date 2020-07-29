79f7bb701f83781254b48ca75d5677919d7678f6
Ippei Tanaka <itanaka@Ippeis-MBP.lan>
2020-07-29 14:51:06 -0400

    Update

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,47 @@ export type Routes<T> = {
     [key:string]: (matches:Matches, ...rest:any) => T
 };
 
+type RouteHandler<R> = {
+  matcher: (path:string) => Matches | null,
+  handler: (matches:Matches, ...rest:any) => R,
+};
+
+export class Router<R>
+{
+  private router:RouteHandler<R>[] = [];
+
+  constructor (routes:Routes<R>)
+  {
+    for (let key of Object.keys(routes))
+    {
+      this.addRoute(key, routes[key]);
+    }
+  }
+
+  addRoute (path:string, callback:(matches:Matches, ...rest:any) => R)
+  {
+    this.router = [...this.router, {
+      matcher: buildMatcher(path),
+      handler: callback
+    }];
+  }
+
+  findRoute (path:string, ...rest:any)
+  {
+    for (let i = 0; i < this.router.length; i++)
+    {
+        const {matcher, handler} = this.router[i];
+        const matchedVars = matcher(path);
+        if (matchedVars) {
+            const {params, wildcards} = matchedVars;
+            return handler({params, wildcards}, ...rest);
+        }
+    }
+    return null;
+  }
+}
+
+/*
 export const buildRouter = (routes:Routes<any>|null) =>
 {
     if (typeof routes !== 'object' || routes === null) {
@@ -29,3 +70,4 @@ export const buildRouter = (routes:Routes<any>|null) =>
         return null;
     };
 };
+*/
--- test/router.test.ts
+++ test/router.test.ts
@@ -1,8 +1,8 @@
-import {buildRouter} from '../src/router';
+import {Router} from '../src/router';
 
 describe('buildRouter', () => {
     it('should take an route object as an argument of the constructor', () => {
-        const router = buildRouter({
+        const router = new Router({
             '/': () => {
                 return 'root';
             },
@@ -11,18 +11,18 @@ describe('buildRouter', () => {
             }
         });
 
-        const value1 = router('/');
+        const value1 = router.findRoute('/');
         expect(value1).toBe('root');
 
-        const value2 = router('/test');
+        const value2 = router.findRoute('/test');
         expect(value2).toBe('test');
 
-        const value3 = router('/else');
+        const value3 = router.findRoute('/else');
         expect(value3).toBeNull();
     });
 
     it('should pick the first route if there are multiple matching routes.', () => {
-        const router = buildRouter({
+        const router = new Router({
             '/test' : () => {
                 return 'a';
             },
@@ -31,59 +31,59 @@ describe('buildRouter', () => {
             }
         });
 
-        const value = router('/test');
+        const value = router.findRoute('/test');
         expect(value).toBe('a');
     });
 
     it('should receive parameters.', () => {
-        const router = buildRouter({
+        const router = new Router({
             '/users/:userId/comments/:commentId'
                 : ({params: {userId, commentId}}) => `${userId} ${commentId}`
         });
 
-        const value = router('/users/abc/comments/dfg');
+        const value = router.findRoute('/users/abc/comments/dfg');
         expect(value).toBe('abc dfg');
     });
 
     it('should receive wildcards.', () => {
 
-        const childRouter = buildRouter({
+        const childRouter = new Router({
             '/comments/:commentId' : ({params: {commentId}}) => {
                 return `<span>comment id :${commentId}</span>`;
             }
         });
 
-        const router = buildRouter({
+        const router = new Router({
             '/users/:userId/*' : ({wildcards}) => {
-                return '<div>user id: ${userId} and ' + childRouter(wildcards[0]) + '</div>';
+                return '<div>user id: ${userId} and ' + childRouter.findRoute(wildcards[0]) + '</div>';
             }
         });
 
-        const value = router('/users/abc/comments/dfg');
+        const value = router.findRoute('/users/abc/comments/dfg');
         expect(value).toBe('<div>user id: ${userId} and <span>comment id :dfg</span></div>');
     });
 
     it('should deal with async handlers.', async () => {
 
-        const router = buildRouter({
+        const router = new Router<Promise<string>|string>({
             '/' : async () => 'hey!',
             '/b' : () => 'hey b!',
         });
 
-        expect(router('/')).toHaveProperty('then');
-        expect(await router('/')).toBe('hey!');
-        expect(router('/b')).toBe('hey b!');
-        expect(await router('/b')).toBe('hey b!');
+        expect(router.findRoute('/')).toHaveProperty('then');
+        expect(await router.findRoute('/')).toBe('hey!');
+        expect(router.findRoute('/b')).toBe('hey b!');
+        expect(await router.findRoute('/b')).toBe('hey b!');
     });
 
     it('should send arguments to callbacks.', async () => {
 
-        const router = buildRouter({
+        const router = new Router({
             '/' : (_, name, size) => {
                 return `My name is ${name} and my size is ${size}`;
             }
         });
 
-        expect(router('/', 'ippei', '1cm')).toBe('My name is ippei and my size is 1cm');
+        expect(router.findRoute('/', 'ippei', '1cm')).toBe('My name is ippei and my size is 1cm');
     });
 });
